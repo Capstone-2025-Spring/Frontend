ca94280409782105ffba84750cc36b5dc78207d3
kimseongsun <kimseongson@gmail.com>
2025-03-22 19:56:00 +0900

    conflict resolution try2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import CounterContainer from "./container/CounterContainer";
 const App = () => {
   return (
     <div>
-      <h1>Redux-Saga Counter</h1>
       <h1>Zustand 기반 Counter</h1>
       <CounterContainer />
     </div>
